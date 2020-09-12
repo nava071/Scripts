e3cccb5f39bad2641ca021b6c401bf2cc4a79c5d
Navaneethan <nava071@gmail.com>
2020-09-12 19:46:09 +0530

    Task1

---
+++ launchapps-notes.md
@@ -0,0 +1,5 @@
+## launchapps.bat
+* chrome browser with multiple profiles
+
+The system will use the last closed chrome browser profile to 
+launch next time.
---
+++ launchapps.bat
@@ -0,0 +1,15 @@
+start C:\path-to-slack\slack.exe
+TIMEOUT 5
+start outlook.exe
+TIMEOUT 5
+start firefox.exe https://www.google.com/
+TIMEOUT 5
+start notepad++.exe
+TIMEOUT 5
+start chrome.exe
+start https://www.python.org/
+start https://www.docker.com/
+TIMEOUT 5
+start explorer.exe
+TIMEOUT 5
+end
